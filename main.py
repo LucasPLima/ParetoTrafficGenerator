d7a56f980fd5506a8a762c224ed7fbda515cc85c
Lucas Pereira Lima <lucas.plima.ti@gmail.com>
2018-01-15 13:29:27 -0300

    Function creation in tsk_analyze.
    Additional function named recv_list, add new matrix in main.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def main():
     x = (int(input('Select the graph number:')))
     tgff = open("tgff_files/creds1.tgff", "r")
     tgff_n = tgff.readlines()
-    sendMatrix = open('outputMatrixes/SendMatrixGraph{}'.format(x), 'w')
-    receiveMatrix = open('outputMatrixes/ReceiveMatrixGraph{}'.format(x), 'w')
+    send_matrix = open('outputMatrixes/SendMatrixGraph{}'.format(x), 'w')
+    receive_matrix = open('outputMatrixes/ReceiveMatrixGraph{}'.format(x), 'w')
     inf = []  # Lista que irá armazenar as informações do grafo selecionado.
-    tasks, indTasks= [], []  # tasks:lista que irá armazenar o número de tasks encontradas em inf[]|indTasks = lista de tasks independentes.|
-    indIndex, depIndex= [],[] #indIndex=Vetor que armazena os índices de tasks independentes. | #depIndex=Vetor que armazena os índices de tasks dependentes
+    tasks, ind_tasks= [], []  # tasks:lista que irá armazenar o número de tasks encontradas em inf[]|indTasks = lista de tasks independentes.|
+    ind_index, dep_index= [],[] #indIndex=Vetor que armazena os índices de tasks independentes. | #depIndex=Vetor que armazena os índices de tasks dependentes
     dep = []  # Lista de dependências de tasks.
 
     t = re.compile('TASK t\d+_\d+')  # Operador de expressões regulares usado para encontrar as tasks.
@@ -21,27 +21,31 @@ def main():
     for i in range(len(tgff_n)):
         tgff_n[i] = tgff_n[i].splitlines()[0]  # Operação de retirada de \n das linhas do arquivo.
 
-    inf = tsk_analyze.graphSelect(tgff_n, inf, x)
-    tasks, dep = tsk_analyze.infExtract(inf, tasks, dep, t, d)
-    matDepSend = tsk_analyze.matrixCreate(len(tasks))
-    matDepSend = tsk_analyze.destList(tasks, dep, matDepSend)
-    indTasks = tsk_analyze.extractInd(tasks, matDepSend, indTasks)
-    indIndex = tsk_analyze.indIndexes(indTasks)
-    tsk_analyze.outputMatrix(matDepSend, tasks, sendMatrix)
+    inf = tsk_analyze.graph_select(tgff_n, inf, x)
+    tasks, dep = tsk_analyze.inf_extract(inf, tasks, dep, t, d)
+    mat_dep_send, mat_dep_receive = tsk_analyze.matrix_create(len(tasks)), tsk_analyze.matrix_create(len(tasks))
+    mat_dep_send = tsk_analyze.dest_list(tasks, dep, mat_dep_send)
+    mat_dep_receive = tsk_analyze.recv_list(mat_dep_send,mat_dep_receive,tasks,x)
+    ind_tasks = tsk_analyze.extract_ind(tasks, mat_dep_send, ind_tasks)
+    ind_index = tsk_analyze.ind_indexes(ind_tasks)
+
+    tsk_analyze.output_matrix(mat_dep_send, tasks, send_matrix)
+    tsk_analyze.output_matrix(mat_dep_receive, tasks, receive_matrix)
 
     ##Pareto.paretoCalculate()
 
-    for i in indIndex:
-        depIndex = tsk_analyze.depIndexes(i,matDepSend)
-        createFile.Envio(i, len(matDepSend[i]), depIndex)
+    for i in ind_index:
+        dep_index = tsk_analyze.dep_indexes(i, mat_dep_send)
+        createFile.Envio(i, len(mat_dep_send[i]), dep_index)
 
-    print('Indexes of IT:{}'.format(indIndex))
-    print('Number of dependent tasks of task{}: {}'.format(indIndex[0], len(matDepSend[indIndex[0]])))
-    print('Dependents of task{}:{}'.format(indIndex[0], depIndex))
+    print('Indexes of IT:{}'.format(ind_index))
+    print('Number of dependent tasks of task{}: {}'.format(ind_index[0], len(mat_dep_send[ind_index[0]])))
+    print('Dependents of task{}:{}'.format(ind_index[0], dep_index))
+    print(tasks)
 
     tgff.close()
-    sendMatrix.close()
-    receiveMatrix.close()
+    send_matrix.close()
+    receive_matrix.close()
 
 
 if __name__ == '__main__':
--- tgff_op/tsk_analyze.py
+++ tgff_op/tsk_analyze.py
@@ -1,70 +1,86 @@
-def matrixCreate(nl):
-    matriz = []
+def matrix_create(nl):
+    matrix = []
     for i in range(nl):
         l = []
-        matriz.append(l)
-    return matriz
+        matrix.append(l)
+    return matrix
 
-def graphSelect (fileList , graphList,graph_n):
-    for i in range(len(fileList)):
-        if fileList[i] == '@TASK_GRAPH {}'.format(graph_n) + ' {':
-            while (fileList[i] != '}'):
-                graphList.append(fileList[i])  # Anexo das informações do grafo selecionado.
+
+def graph_select(file_list, graph_list, graph_n):
+    for i in range(len(file_list)):
+        if file_list[i] == '@TASK_GRAPH {}'.format(graph_n) + ' {':
+            while file_list[i] != '}':
+                graph_list.append(file_list[i])  # Anexo das informações do grafo selecionado.
                 i += 1
             else:
                 break
-    return graphList
+    return graph_list
 
-def infExtract (graphList,taskList,depList,taskPattern,depPattern):
-    for i in graphList:
-        if len(taskPattern.findall(i))==1:
-            taskList.append(taskPattern.findall(i)[0].split('TASK ')[1])  #Extrai as tasks existentes em inf.
-        if len(depPattern.findall(i))==1:
-            depList.append(depPattern.findall(i)[0].split('FROM ')[1].split('  TO  '))  #Extrai as dependências das tasks em inf.
-    return taskList,depList
 
-def destList(taskList,depList,matDep):
-    for i in range(len(taskList)):
-        for j in range(len(depList)):
-            if taskList[i] == depList[j][0]: #compara as tarefas ao índice de dependências para criar uma lista de destinatários, com base nas tarefas relacionadas.
-                matDep[i].append(depList[j][1]) #indexa ao índice correspondente a tarefa, seus destinatários.
-    return matDep
+def inf_extract(graph_list, task_list, dep_list, task_pattern, dep_pattern):
+    for i in graph_list:
+        if len(task_pattern.findall(i)) == 1:
+            task_list.append(task_pattern.findall(i)[0].split('TASK ')[1])  # Extrai as tasks existentes em inf.
+        if len(dep_pattern.findall(i)) == 1:
+            dep_list.append(
+                dep_pattern.findall(i)[0].split('FROM ')[1].split('  TO  '))  # Extrai as dependências das tasks em inf.
+    return task_list, dep_list
 
-def extractInd(taskList,matDep,indList):
-    ocorrencies = 0
-    for i in range(len(taskList)):
-        ocorrencies = 0 #variável que conta o número de repetições de um elemento i de taskList na matriz de dependências.
-        for j in range(len(matDep)):
-            for k in range(len(matDep[j])):
-                if taskList[i] == matDep[j][k]: #compara os elementos de taskList com cada elemento da matriz.
-                    ocorrencies +=1
-        if ocorrencies==0:
-            indList.append(taskList[i])
-    return indList
 
-def outputMatrix(matDep,tasks,arq):
-    for i in range(len(matDep)):
-        if (len(matDep[i]) > 0):
-            for j in range(len(matDep[i])):
-                inf = '{}\t'.format(matDep[i][j])
+def dest_list(task_list, dep_list, mat_dep):
+    for i in range(len(task_list)):
+        for j in range(len(dep_list)):
+            if task_list[i] == dep_list[j][0]:  # compara as tarefas ao índice de dependências para criar uma lista de destinatários, com base nas tarefas relacionadas.
+                mat_dep[i].append(dep_list[j][1])  # indexa ao índice correspondente a tarefa, seus destinatários.
+    return mat_dep
+
+
+def recv_list(mat_dep_send, mat_dep_receive, task_list, graph_ind):
+    for i in range(len(task_list)):
+        for j in range(len(mat_dep_send)):
+            for k in range(len(mat_dep_send[j])):
+                if(task_list[i]==mat_dep_send[j][k]):
+                    mat_dep_receive[i].append('t{}_{}'.format(graph_ind, j))
+    return mat_dep_receive
+
+
+def extract_ind(task_list, mat_dep, ind_list):
+    for i in range(len(task_list)):
+        ocorrencies = 0  # variável que conta o número de repetições de um elemento i de taskList na matriz de dependências.
+        for j in range(len(mat_dep)):
+            for k in range(len(mat_dep[j])):
+                if task_list[i] == mat_dep[j][k]:  # compara os elementos de taskList com cada elemento da matriz.
+                    ocorrencies += 1
+        if ocorrencies == 0:
+            ind_list.append(task_list[i])
+    return ind_list
+
+
+def output_matrix(mat_dep, tasks, arq):
+    for i in range(len(mat_dep)):
+        if len(mat_dep[i]) > 0:
+            for j in range(len(mat_dep[i])):
+                inf = '{}\t'.format(mat_dep[i][j])
                 arq.write(inf)
-            for z in range((len(tasks)-1)-len(matDep[i])):
+            for z in range((len(tasks) - 1) - len(mat_dep[i])):
                 inf = '0\t\t'
                 arq.write(inf)
         else:
-            for j in range(len(tasks)-1):
+            for j in range(len(tasks) - 1):
                 inf = '0\t\t'
                 arq.write(inf)
         arq.write('\n')
 
-def indIndexes(indTasks):
+
+def ind_indexes(ind_tasks):
     indexes = []
-    for i in range(len(indTasks)):
-        indexes.append(int(indTasks[i].split('_')[1]))
+    for i in range(len(ind_tasks)):
+        indexes.append(int(ind_tasks[i].split('_')[1]))
     return indexes
 
-def depIndexes(indTask, matDep):
-    dep=[]
-    for i in range(len(matDep[indTask])):
-        dep.append(int(matDep[indTask][i].split('_')[1]))
+
+def dep_indexes(ind_task, mat_dep):
+    dep = []
+    for i in range(len(mat_dep[ind_task])):
+        dep.append(int(mat_dep[ind_task][i].split('_')[1]))
     return dep
