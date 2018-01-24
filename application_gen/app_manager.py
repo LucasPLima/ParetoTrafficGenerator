ce427d4c52df7649b0c1af797129a7436c137b21
Lucas Pereira Lima <lucas.plima.ti@gmail.com>
2018-01-24 13:14:08 -0300

    App_manager update.
    Different versions of application are generated now.

--- application_gen/app_manager.py
+++ application_gen/app_manager.py
@@ -1,19 +1,67 @@
 from application_gen import node_create
-import os,errno
+from shutil import copy2
+import os
+import errno
 
 
-def create_versions(ind_index,mat_dep_send,mat_dep_receive, local,i):
-
+def create_versions(ind_index, mat_dep_send, mat_dep_receive, local, i):
     try:
         os.makedirs(local)
-        new_local = os.path.dirname('{}/app{}_0/'.format(local, i))
-        os.makedirs(new_local)
+        f_version = os.path.dirname('{}/app{}_0/'.format(local, i))
+        os.makedirs(f_version)
 
-        node_create.ind_nodes(ind_index, mat_dep_send, new_local)
-        node_create.dep_nodes(mat_dep_send, mat_dep_receive, ind_index, new_local)
-    except OSError as e:
-        new_local = os.path.dirname('{}/app{}_0/'.format(local, i))
-        node_create.ind_nodes(ind_index, mat_dep_send, new_local)
-        node_create.dep_nodes(mat_dep_send, mat_dep_receive, ind_index, new_local)
-        if e.errno != errno.EEXIST:
+        node_create.ind_nodes(ind_index, mat_dep_send, f_version)
+        node_create.dep_nodes(mat_dep_send, mat_dep_receive, ind_index, f_version)
+
+        files = next(os.walk(f_version))[2]
+
+        for j in range(1, 33):
+            try:
+                o_version = os.path.dirname('{}/app{}_{}/'.format(local, i, j))
+                os.makedirs(o_version)
+
+                for k in files:
+                    src = '{}/{}'.format(f_version, k)
+                    copy2(src, o_version)
+                node_create.ind_nodes(ind_index, mat_dep_send, o_version)
+
+            except OSError as e:
+                o_version = os.path.dirname('{}/app{}_{}/'.format(local, i, j))
+
+                for k in files:
+                    src = '{}/{}'.format(f_version, k)
+                    copy2(src, o_version)
+                node_create.ind_nodes(ind_index, mat_dep_send, o_version)
+                if e.errno != errno.EEXIST:
+                    raise
+
+    except OSError as er:
+        f_version = os.path.dirname('{}/app{}_0/'.format(local, i))
+
+        node_create.ind_nodes(ind_index, mat_dep_send, f_version)
+        node_create.dep_nodes(mat_dep_send, mat_dep_receive, ind_index, f_version)
+
+        files = next(os.walk(f_version))[2]
+
+        for j in range(1, 33):
+            try:
+                o_version = os.path.dirname('{}/app{}_{}/'.format(local, i, j))
+                os.makedirs(o_version)
+
+                for k in files:
+                    src = '{}/{}'.format(f_version, k)
+                    copy2(src, o_version)
+                node_create.ind_nodes(ind_index, mat_dep_send, o_version)
+
+            except OSError as e:
+                o_version = os.path.dirname('{}/app{}_{}/'.format(local, i, j))
+
+                for k in files:
+                    src = '{}/{}'.format(f_version, k)
+                    copy2(src, o_version)
+                node_create.ind_nodes(ind_index, mat_dep_send, o_version)
+                if e.errno != errno.EEXIST:
+                    raise
+
+        if er.errno != errno.EEXIST:
             raise
